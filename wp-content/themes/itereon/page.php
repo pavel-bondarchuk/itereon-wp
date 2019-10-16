457889dd5d51be24da7e7a934f560f9c7512471f
aosmichenko <shurikx3m@gmail.com>
2019-10-16 18:34:32 +0200

    remove default loop, no needed on single

--- wp-content/themes/itereon/page.php
+++ wp-content/themes/itereon/page.php
@@ -13,54 +13,47 @@
  */
 
 get_header();
+the_post();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
 
-			<?php
-			while ( have_posts() ) :
-				the_post();
-				?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-					</header><!-- .entry-header -->
+				<?php itereon_post_thumbnail(); ?>
 
-					<?php itereon_post_thumbnail(); ?>
+				<div class="entry-content">
+					<?php
+					the_content();
 
-					<div class="entry-content">
+					wp_link_pages( [
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 's_itereon' ),
+						'after'  => '</div>',
+					] );
+					?>
+				</div><!-- .entry-content -->
+
+				<?php if ( get_edit_post_link() ) : ?>
+					<footer class="entry-footer">
 						<?php
-						the_content();
-
-						wp_link_pages( [
-							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 's_itereon' ),
-							'after'  => '</div>',
-						] );
+						edit_post_link( sprintf( wp_kses( /* translators: %s: Name of current post. Only visible to screen readers */ __( 'Edit <span class="screen-reader-text">%s</span>', 's_itereon' ), [
+							'span' => [
+								'class' => [],
+							],
+						] ), get_the_title() ), '<span class="edit-link">', '</span>' );
 						?>
-					</div><!-- .entry-content -->
+					</footer><!-- .entry-footer -->
+				<?php endif; ?>
+			</article><!-- #post-<?php the_ID(); ?> -->
+			<?php
 
-					<?php if ( get_edit_post_link() ) : ?>
-						<footer class="entry-footer">
-							<?php
-							edit_post_link( sprintf( wp_kses( /* translators: %s: Name of current post. Only visible to screen readers */
-								__( 'Edit <span class="screen-reader-text">%s</span>', 's_itereon' ), [
-									'span' => [
-										'class' => [],
-									],
-								] ), get_the_title() ), '<span class="edit-link">', '</span>' );
-							?>
-						</footer><!-- .entry-footer -->
-					<?php endif; ?>
-				</article><!-- #post-<?php the_ID(); ?> -->
-				<?php
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 			?>
 
 		</main><!-- #main -->
--- wp-content/themes/itereon/single.php
+++ wp-content/themes/itereon/single.php
@@ -8,15 +8,13 @@
  */
 
 get_header();
+the_post();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
+			<?php
 			get_template_part( 'template-parts/content', get_post_type() );
 
 			the_post_navigation();
@@ -26,8 +24,7 @@ get_header();
 				comments_template();
 			endif;
 
-		endwhile; // End of the loop.
-		?>
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
